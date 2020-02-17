0bd80b7a9cb74a898e1c4588c6ce4d47c9907b38
Th0rn <11_ka@rambler.ru>
2020-02-17 19:00:48 +0300

    в заданиях не должно быть выполняемого кода, исправил

---
+++ cases/squirrel.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def squirrel(N):
+    """Function return first number of factorial(N)"""
+
+    def factorial(num):
+        """Recursive calculate factorial N"""
+        if type(num) == int and num >= 0:
+            result = lambda num: result(num - 1) * num if num > 0 else 1
+            return result(num)
+        else:
+            raise TypeError(
+                "The given value must be positive integer, not %s" % (
+                    type(num) if type(num) != int
+                    else "negative"))
+    return str(factorial(N))[0]
+
