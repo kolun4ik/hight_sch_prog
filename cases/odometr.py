4d92ea28e0babd64748f1c57215107b8ae4a3270
Th0rn <11_ka@rambler.ru>
2020-02-19 19:12:44 +0300

    Поездка на мотоцикле

---
+++ cases/odometr.py
@@ -0,0 +1,8 @@
+def odometr(data = list()):
+    distance = 0
+    if (len(data) >= 2) and (len(data) % 2 == 0):
+        for counter, value in enumerate(data):
+            if counter % 2 == 0:
+                distance += value
+        return distance
+    return False
