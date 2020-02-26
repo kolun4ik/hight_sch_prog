0a828b542df296b16b5c0b4d66895b0aab9b01e8
Th0rn <11_ka@rambler.ru>
2020-02-26 19:24:36 +0300

    refactoring odometr.py

--- cases/odometr.py
+++ cases/odometr.py
@@ -1,8 +1,7 @@
-def odometr(data = list()):
+def odometer(data = list()):
     distance = 0
     if (len(data) >= 2) and (len(data) % 2 == 0):
         for counter, value in enumerate(data):
             if counter % 2 == 0:
                 distance += value
-        return distance
-    return False
+        return int(distance)
